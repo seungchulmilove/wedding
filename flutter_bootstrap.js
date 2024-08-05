235efd20b3972c18ab9843d8d99593bcdba5a849
alaxhenry <alaxhenry0121@gmail.com>
2024-08-05 23:00:00 +0900

    d 데이 추가

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4218945543"
+    serviceWorkerVersion: "2734019347"
   }
 });
